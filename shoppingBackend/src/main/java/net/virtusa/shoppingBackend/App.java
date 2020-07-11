1436bc9bf17b7a3224e8bea2b46b0e3a86822bdd
virtusaproject <tawarepranjali111@gmail.com>
2020-07-10 18:56:50 -0700

    First commit

---
+++ shoppingBackend/src/main/java/net/virtusa/shoppingBackend/App.java
@@ -0,0 +1,13 @@
+package net.virtusa.shoppingBackend;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
